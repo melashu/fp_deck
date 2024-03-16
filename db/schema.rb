0c047b69911f27c17ffc7b37b3d943e20d5d1ac3
Melashu Amare <meshu.amare@gmail.com>
2024-03-16 22:04:42 +0300

    Rename type to user_type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_16_163835) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_16_183700) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,7 +23,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_16_163835) do
 
   create_table "trading_accounts", force: :cascade do |t|
     t.string "name"
-    t.string "type"
+    t.string "user_type"
     t.string "phase"
     t.bigint "status_id", null: false
     t.bigint "user_id", null: false
@@ -48,13 +48,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_16_163835) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "role"
+    t.string "role", default: "user"
     t.string "first_name"
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
   add_foreign_key "trading_accounts", "statuses"
