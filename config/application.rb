bbea86fb1ad30145e27e90d781a0d1ccad0fe956
Melashu Amare <meshu.amare@gmail.com>
2024-03-16 19:04:39 +0300

    Add rspec

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module FpDeck
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-
+    config.generators.test_framework = :rspec
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
