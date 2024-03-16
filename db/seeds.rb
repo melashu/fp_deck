bcbc9b0cf58255123a41e27fa1e5f9899dff0165
Melashu Amare <meshu.amare@gmail.com>
2024-03-16 21:44:15 +0300

    Create data for seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,32 @@
-# frozen_string_literal: true
+User.user.delete_all
+Status.all.delete_all
+TradingAccount.all.delete_all
+puts "=======================Started====================="
+TYPE = ["standard", "algo", "competition"]
+PHASE = ["student", "practitioner", "master"]
+20.times do 
+    User.create!(first_name: Faker::Name.first_name,email: Faker::Internet.email, last_name: Faker::Name.last_name, user_name: Faker::Name.unique.first_name, password: "12345678")
+end
+puts "=======================Succssfuly create 20 users====================="
 
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Status.create!(name: 'new')
+Status.create!(name: 'ongoing')
+Status.create!(name: 'not_passed')
+Status.create!(name: 'passed')
+Status.create!(name: 'in_review')
+Status.create!(name: 'onboarding')
+Status.create!(name: 'denied')
+Status.create!(name: 'expired')
+puts "=======================Succssfuly create 8 statuses====================="
+
+100.times do
+    TradingAccount.create!(name: Faker::FunnyName.unique.name, user_type: TYPE[rand(0..2)], phase: PHASE[rand(0..2)], status_id: Status.order("RANDOM()").last.id, started: rand(1..5).day.ago, ended: rand(1..5).day.ago, user_id: User.order("RANDOM()").last.id, size: rand(1..100)*1000, balance:rand(1..100)*1000,profit: rand(1..100)*100,equity: rand(1..100)*100, op: rand(1..1000), payouts: rand(1..10_000))
+end
+puts "=======================Succssfuly create 100 trading accounts====================="
+
+puts "=======================Finished====================="
+
+
+
+
+
