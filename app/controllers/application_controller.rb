a339c4a18cdc11fc511a283a5c19781daa64c4e0
Melashu Amare <meshu.amare@gmail.com>
2024-03-16 22:04:20 +0300

    Add devise authenticate_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up,
+                                          keys: %i[email password password_confirmation first_name last_name role])
+        devise_parameter_sanitizer.permit(:sign_in, keys: %i[email password])
+        devise_parameter_sanitizer.permit(:account_update,
+                                          keys: %i[email password password_confirmation first_name last_name role])
+      end
 end
