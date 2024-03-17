5253862cdb4814abfe2949647efc80b447ae5eea
Melashu Amare <meshu.amare@gmail.com>
2024-03-18 01:23:24 +0300

    Add user

--- app/components/search_filter_component.rb
+++ app/components/search_filter_component.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class SearchFilterComponent < ViewComponent::Base
-    def initialize(search_filter:)
+    def initialize(search_filter:, user:)
+        @user = user
         @search_filter = search_filter
-        @saved_filters = [["Saved Filters", "Saved Filters"]]
-        @saved_filters << SearchFilter.all.map{|filter| [filter.name, filter.id]}
+        @saved_filters = SearchFilter.all.map{|filter| [filter.name, filter.id]} << ["Saved Filters", "Saved Filters"]
         @statuses = Status.all.map{|status| [status.name, status.id]}
         @types = [["all", "all"], ["standard", "standard"], ["algo", "algo"], ["competition", "competition"]]
         @pases = [["all", "all"],["student", "student"], ["practitioner", "practitioner"], ["master", "master"]]
