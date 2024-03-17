2a263c0cb196400304f376b0dc96dc994eaa99e6
Melashu Amare <meshu.amare@gmail.com>
2024-03-18 01:29:32 +0300

    Add stimulus notification

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,6 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js'
 pin '@hotwired/stimulus', to: 'stimulus.min.js'
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
+pin "stimulus-notification" # @2.2.0
+pin "hotkeys-js" # @3.13.7
+pin "stimulus-use" # @0.51.3
