09a9bb2d5a1b3d500fa2919c97206752a4e89f15
Melashu Amare <meshu.amare@gmail.com>
2024-03-19 08:30:02 +0300

    Add  has many trading_accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+        has_many :trading_accounts
     enum role: {
       user: "user",
       admin: "admin"
