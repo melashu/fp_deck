3d407d1dcfc1201114f255e3fdbe9f39226e374e
Melashu Amare <meshu.amare@gmail.com>
2024-03-19 08:29:04 +0300

    Add filter action

--- app/controllers/trading_accounts_controller.rb
+++ app/controllers/trading_accounts_controller.rb
@@ -3,4 +3,11 @@ class TradingAccountsController < ApplicationController
     def index
      @pagy, @trading_accounts = pagy(TradingAccount.trading_account_list(params))
     end
+
+    def filter
+        @number_of_record = TradingAccount.trading_account_list(params).count
+         respond_to do |format|
+             format.turbo_stream
+         end
+     end
 end
