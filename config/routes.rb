a57e56b6ef63964a660dc65126cc868d60698ae3
Melashu Amare <meshu.amare@gmail.com>
2024-03-19 08:35:54 +0300

    Add filter to the route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
-  resources :trading_accounts
+  resources :trading_accounts do
+    collection do
+      post "filter"
+    end
+  end
   resources :search_filters do
     member do
       get "pre_populate"
